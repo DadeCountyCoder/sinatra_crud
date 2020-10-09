60bca565ce29b29baf49763d5d3d63ac85a0dfa1
DadeCountyCoder <dadecountycoder@gmail.com>
2020-10-09 13:12:49 -0400

    fill_tables

--- food_to_go/db/migrate/20201009165038_create_orders_table.rb
+++ food_to_go/db/migrate/20201009165038_create_orders_table.rb
@@ -1,4 +1,10 @@
 class CreateOrdersTable < ActiveRecord::Migration
   def change
+    create_tables :orders do |t|
+      t.string :address
+      t.string :item
+      t.integer :item-price
+      t.integer :total
+    end
   end
 end
